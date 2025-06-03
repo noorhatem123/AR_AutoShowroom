d4de57a425a1333e6552ce3070c5abb24adf0367
noorhatem123 <noor.kadry22d@eslsca.edu.eg>
2025-06-03 23:42:51 +0300

    fixed scene loader

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,15 +5,18 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadMarkerlessScene()
     {
+        Resources.UnloadUnusedAssets();
         SceneManager.LoadScene("MarkerlessARScene");
     }
 
     public void LoadMarkerBasedScene()
     {
+        Resources.UnloadUnusedAssets();
         SceneManager.LoadScene("MarkerBasedScene");
     }
     public void LoadStartupScene()
     {
+        Resources.UnloadUnusedAssets();
         UnityEngine.SceneManagement.SceneManager.LoadScene("StartupScene");
     }
 }
